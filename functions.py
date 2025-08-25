6f35776484ab173de965bb61515a88074298188f
Diogo Neves <nevesdiogo2006@gmail.com>
2025-08-25 20:34:45 +0100

    Function Comments

--- functions.py
+++ functions.py
@@ -1,10 +1,11 @@
 from github import Github
 
 def create_message(project: str, prompt: str, sha: str) -> str:
-    "Given a prompt and a sha from a commit, gets the .diff of that commit and creates a message for the IA"
+    "Given a prompt and a sha from a commit, gets the commit and creates a message for the IA"
    
     g = Github()
     
+    # Gets the repository 
     if project == "Linux":
         repo = g.get_repo("torvalds/linux")
     elif project == "Mozilla":
@@ -12,15 +13,17 @@ def create_message(project: str, prompt: str, sha: str) -> str:
     elif project == "Xen":
         repo = g.get_repo("xen-project/xen")
     commit = repo.get_commit(sha)
-    response_format = "Respond only in the format:\nDefect Type: ...\nDefect Qualifier: ..."
+    response_format = "Respond only in the format:\nDefect Type: <Defect Type>\nDefect Qualifier: <Defect Qualifier>"
     
+    # Write commit in txt file
     with open("prompt.txt", "w") as p:
         for f in commit.files:
             p.write("\nFile name: " + f.filename + "\n")
             p.write("Changes: " + str(f.changes) + "\n")
             p.write("Patch (diff):\n" + f.patch + "\n")
 
+    # Joins the prompt with the commit and the format intended
     with open("prompt.txt", "r") as p:
-        content = prompt + "\n" + p.read() + "\n" + response_format  # Junta a prompt e o commit
+        content = prompt + "\n" + p.read() + "\n" + response_format  
     
     return content
