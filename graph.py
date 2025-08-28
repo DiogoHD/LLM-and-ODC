408b9006a4083f5a059a4adc044afdac4ba8e975
Diogo Neves <nevesdiogo2006@gmail.com>
2025-08-28 19:04:05 +0100

    Values on top of the bars

--- graph.py
+++ graph.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-outputs = {}
-
 df = pd.read_csv("file.csv")
+
 
 # Defect Type Bar Plot
 keys_type = list(set(df["Defect Type"]))
@@ -11,6 +10,9 @@ df_type  = df["Defect Type"].value_counts()
 plt.subplot(1, 2, 1)
 df_type.plot(kind = "bar")
 plt.title("Defect Type - Count")
+# Adds values on top of the bars
+for i, value in enumerate(df_type):
+    plt.text(i, value, str(value), ha='center', va='bottom')
 
 # Defect Qualifier Bar Plot
 keys_qualifier = list(set(df["Defect Qualifier"]))
@@ -18,6 +20,10 @@ df_qualifier = df["Defect Qualifier"].value_counts()
 plt.subplot(1, 2, 2)
 df_qualifier.plot(kind = "bar")
 plt.title("Defect Qualifier - Count")
+# Adds values on top of the bars
+for i, value in enumerate(df_qualifier):
+    plt.text(i, value, str(value), ha='center', va='bottom')
+
 
 plt.suptitle("Results")
 plt.show()
