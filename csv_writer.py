35b6c0d6ebcdd0bfbcc7caf21535b87b0b3233a6
Diogo Neves <nevesdiogo2006@gmail.com>
2025-08-28 19:05:05 +0100

    regex.verbose and allowing word between brackets

--- csv_writer.py
+++ csv_writer.py
@@ -2,6 +2,22 @@ import csv
 import os
 import regex
 
+def make_pattern(name: str):
+    # Uses a raw string so Python can allow escape characters
+    pattern = regex.compile(rf"""
+    (?:{name})      # Use that word witouth capturing it
+    {{e<=1}}        # Fuzzy Matching - Allows at most 1 typo (only possible using the module regex (impossible with re))
+    \s*[:\-–—]\s*   # Allows various separators and it can have 0 or multiple spaces before or after the separator
+    (?:\d+\)?\s*)?  # If a number appears before the word that we want [2) or 3] it ignores it
+    [(<[\{{]*       # Allows the word to be between some kind of brackets
+    ([A-Za-z/]+)    # Caputres the first word after the string (that only can contain letters and a /)
+    """, regex.IGNORECASE | regex.VERBOSE)
+    # regex.IGNORECASE makes the search be case-insensitive
+    # regex.VERBOSE makes the search ignore newlines, spaces, tabs and comments
+    
+    return pattern
+
+
 folder = "responses"
 
 with open("file.csv", "w", newline='') as csv_file:
@@ -18,16 +34,8 @@ with open("file.csv", "w", newline='') as csv_file:
             with open(f_path, "r") as f:
                 text = f.read()
                 
-                # We now use regex so we can allow errors
-                # (?:   ) -> To use that word witouth capturing it
-                # {e<=1} -> Allows 1 typo
-                # Uses a raw string (r) so Python can allow escape characters
-                # \s* -> Allows spaces
-                # (\S+) -> Caputres the first word after the string
-                # [:\-–—] -> Allows : (Colon), - (Hyphen), – (En dash) and — (Em dash)
-                # regex.IGNORECASE makes the search be case-insensitive
-                type_match = regex.search(r"(?:Defect Type){e<=1}\s*[:\-–—]\s*(\S+)", text, regex.IGNORECASE)
-                qualifier_match = regex.search(r"(?:Defect Qualifier){e<=1}\s*[:\-–—]\s*(\S+)", text, regex.IGNORECASE)
+                type_match = regex.search(make_pattern("Defect Type"), text)
+                qualifier_match = regex.search(make_pattern("Defect Qualifier"), text)
                 
                 # Takes the response found
                 defect_type = type_match.group(1).strip("'\",*()") if type_match else None
