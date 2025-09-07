313aa451872b1d87540c4e20b3531dfd2e3e098d
Diogo Neves <nevesdiogo2006@gmail.com>
2025-09-07 17:06:45 +0100

    Reducing Ax Parameters

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def create_bar(Category: str, axes: plt.axes, ax: int):
+def create_bar(Category: str, ax: plt.axes):
     # Counting Data
     output_counts = df_output[Category].value_counts()
     input_counts  = df_input[Category].value_counts()
@@ -16,22 +16,22 @@ def create_bar(Category: str, axes: plt.axes, ax: int):
     
     # Bar width and x locations
     w, x = 0.4, np.arange(len(keys))
-    axes[ax].bar(x - w/2, output_counts, width=w, label='IA')
-    axes[ax].bar(x + w/2, input_counts, width=w, label='Human')
+    ax.bar(x - w/2, output_counts, width=w, label='IA')
+    ax.bar(x + w/2, input_counts, width=w, label='Human')
     
     # Labels
-    axes[ax].set_xticks(x)
-    axes[ax].set_xticklabels(keys, rotation=45, ha="right")
-    axes[ax].set_ylabel(Category)
-    axes[ax].set_title(Category + " - Comparison")
+    ax.set_xticks(x)
+    ax.set_xticklabels(keys, rotation=45, ha="right")
+    ax.set_ylabel(Category)
+    ax.set_title(Category + " - Comparison")
     
     # Adds values on top of the bars for the IA
     for i, value in enumerate(output_counts):
-        axes[ax].text(x[i] - w/2, value, str(value), ha='center', va='bottom')
+        ax.text(x[i] - w/2, value, str(value), ha='center', va='bottom')
     
     # Adds values on top of the bars for the Human
     for i, value in enumerate(input_counts):
-        axes[ax].text(x[i] + w/2, value, str(value), ha='center', va='bottom')
+        ax.text(x[i] + w/2, value, str(value), ha='center', va='bottom')
     
 
 # Read CSVs
@@ -40,8 +40,8 @@ df_input = pd.read_csv("input.csv")
 
 # Creating Bar Graphs
 fig, axes = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True)    # constrained_layout automatically adjusts the space between subplots, titles, labels and legends
-create_bar("Defect Type", axes, 0)
-create_bar("Defect Qualifier", axes, 1)
+create_bar("Defect Type", axes[0])
+create_bar("Defect Qualifier", axes[1])
 
 fig.legend(['IA', 'Human'], loc='lower center')    # Showing bar graph's legend
 fig.suptitle("Defect Type and Defect Qualifier Comparison", fontsize=16)         # The Main Title
