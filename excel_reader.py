4e4c8dc34b046f65879899f9ad763e186f32c9f8
Diogo Neves <nevesdiogo2006@gmail.com>
2025-09-18 19:08:54 +0100

    Deleting Lines is not needed

--- excel_reader.py
+++ excel_reader.py
@@ -3,13 +3,10 @@ import pandas as pd
 excel = pd.read_excel("vulnerabilities.xlsx", sheet_name=0, header=1)
 excel[["V_ID", "Project"]] = excel[["V_ID", "Project"]].ffill()
 
-desired_cols = ["V_ID", "Project", "CVE", "V_CLASSIFICATION", "P_COMMIT", "Defect Type", "Defect Qualifier", "# Files"]
+desired_cols = ["V_ID", "Project", "CVE", "V_CLASSIFICATION", "P_COMMIT", "Defect Type", "Defect Qualifier", "# Files", "Filenames"]
 
 # Filters the initial excel
 df = excel[desired_cols].copy()
-
-# Deletes this line
-df = df[df["Project"] != "CVE-2007-6151+C7+E7"]
 
 # Filters with vulnerabilites only with 1 file
 df = df[df["# Files"] == 1]
