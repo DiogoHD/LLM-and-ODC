c5c93f3d0af4c9e2bbedc28191bca9dc1b16e53f
Diogo Neves <nevesdiogo2006@gmail.com>
2025-09-27 00:45:51 +0100

    Added docstrings and changed variables names

--- src/functions/commit_utils.py
+++ src/functions/commit_utils.py
@@ -6,7 +6,15 @@ from github import Commit, Github, GithubException
 
 # main.py 
 def fetch_commit(project: str, sha: str) -> Commit.Commit | None:
-    "Given a project and a sha from a commit, fetches the commit"
+    """Given a project and a sha from a commit, fetches the commit
+    
+    Args:
+        project (str): The name of the project
+        sha (str): The sha of the commit (also called P_COMMIT)
+        
+    Returns:
+        Commit.Commit | None: The commit from github
+    """
     
     g = Github()
     
@@ -29,20 +37,29 @@ def fetch_commit(project: str, sha: str) -> Commit.Commit | None:
         print(f"Error accessing repository '{project}' with commit '{sha}': {e}")       # If it can't access the repo or the commit, ir prints an error
         return None
 
-def create_message(commit: Commit.Commit, prompt: str) -> list[tuple[str, str]]:
-    "Given a commit and initial prompt, creates a message from the IA"
+
+def create_message(commit: Commit.Commit, instruction: str) -> list[tuple[str, str]]:
+    """"Given a commit and initial instruction, creates a prompt from the IA
+    
+    Args:
+        commit (Commit.Commit): A commit from a github repository
+        instruction (str): An instruction for the IA that will join with the commit and a intended response format
+        
+    Returns:
+        list[tuple[str, str]]: A list of tuples. Each tuple has a prompt and the name of the file that the prompt was created for
+    """
+    
     if commit is None:
         return []
     
     response_format = "Your response should not provide an explanation and should only contain the following response format for each defect you classify in each file:\nDefect Type: <Defect Type>\nDefect Qualifier: <Defect Qualifier>"  
     # Save prompt for each file from the commit
-    content = []
+    prompts = []
     for f in commit.files:
-        file_prompt = f"{prompt}\n\nFile name: {f.filename}\nChanges: {str(f.changes)}\nPatch (diff):\n{f.patch}\n\n{response_format}"      # Joins the prompt with the commit and the format intended
-        content.append((file_prompt, f.filename))
+        file_prompt = f"{instruction}\n\nFile name: {f.filename}\nChanges: {str(f.changes)}\nPatch (diff):\n{f.patch}\n\n{response_format}"      # Joins the prompt with the commit and the format intended
+        prompts.append((file_prompt, f.filename))
     
-    return content
-
+    return prompts
 
 
 def process_commit(row, prompt: str, models: list[str]) -> None:
@@ -54,6 +71,7 @@ def process_commit(row, prompt: str, models: list[str]) -> None:
         prompt (str): The prompt for the IA
         models (list[str]): A list of the IA models downloaded via ollama
     """
+    
     root_dir = Path(__file__).parent.parent.parent  # Get the root folder
     output_dir = root_dir / "output"                # Joins with output directory
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -73,14 +91,25 @@ def process_commit(row, prompt: str, models: list[str]) -> None:
         for model in models:  
             call_model(model, message, file_dir)
 
-def call_model(model: str, content: str, folder: Path) -> None:
-    "Calls IA model via ollama, runs the specified prompt and stores the response in a text file"
+
+def call_model(model: str, prompt: str, folder: Path) -> None:
+    """Calls IA model via ollama, runs the specified prompt and stores the response in a text file
+    
+    Args:
+        model (str): The name of the IA model that will be run
+        prompt (str): The message that will be given to the IA
+        folder (Path): The folder where the text file will be stored in
+        
+    Raises:
+        RuntimeError: If the writing of the IA response in a text file doesn't work
+    """
+    
     model_name: str = model.partition(":")[0]    # Take model name before ':' if present
     
     try:
         response: ollama.ChatResponse = ollama.chat(
             model = model,                                      # Defines which ollama's model is going to be used
-            messages = [{"role": "user", "content": content}],  # Defines who's using the model and what's going to be its content
+            messages = [{"role": "user", "content": prompt}],   # Defines who's using the model and what's going to be its content
             )
         
         # Writes response in a text file
